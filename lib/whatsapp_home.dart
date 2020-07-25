df8d1ebae2986c6131170b5ad4daed774c5a6016
Jose Bustillo <josebustillodealba@gmail.com>
2020-07-24 20:21:45 -0500

    feat: WhatsAppHome init

---
+++ lib/whatsapp_home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class WhatsAppHome extends StatefulWidget {
+  @override
+  _WhatsAppHomeState createState() => _WhatsAppHomeState();
+}
+
+class _WhatsAppHomeState extends State<WhatsAppHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       appBar: new AppBar(
+         title: new Text('MyChat'),
+       ),
+    );
+  }
+}
