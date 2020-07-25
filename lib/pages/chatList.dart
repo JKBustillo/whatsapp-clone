f1331a303f34449da1c973eb436a2b75b8f15448
Jose Bustillo <josebustillodealba@gmail.com>
2020-07-24 22:54:34 -0500

    feat: listView with the item from ChatModel and image rendered

--- lib/pages/chatList.dart
+++ lib/pages/chatList.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/models/chat_model.dart';
 
-class ChatList extends StatelessWidget {
+class ChatList extends StatefulWidget {
+  @override
+  _ChatListState createState() => _ChatListState();
+}
+
+class _ChatListState extends State<ChatList> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return ListView.builder(
+      itemCount: messageData.length,
+      itemBuilder: (BuildContext context, int i) => new Column(
+        children: <Widget>[
+          new Divider(
+            height: 10.0,
+          ),
+          new ListTile(
+            leading: new CircleAvatar(
+              backgroundImage: NetworkImage(messageData[i].imageURL),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
