00b13211e669963de2230e5f94759bbca7aef5e8
Jose Bustillo <josebustillodealba@gmail.com>
2020-07-28 07:27:47 -0500

    feat: Contacts Widget

---
+++ lib/pages/contacts.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Contacts extends StatefulWidget {
+
+  @override
+  _ContactsState createState() => _ContactsState();
+}
+
+class _ContactsState extends State<Contacts> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       appBar: new AppBar(
+         title: new Text("Select contact")
+       ),
+    );
+  }
+}
